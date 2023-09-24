48ba34959ba91b0438957db08d038ce769278012
Joey Wang <joey840404@gmail.com>
2023-09-24 12:20:56 -0700

    handle keyboardInterrupt

--- src/main.py
+++ src/main.py
@@ -6,23 +6,24 @@ import time
 
 
 def main():
-    print("Initializing...")
-    audio_file_name = "myrecording.wav"
-    start_time = time.time()
-    elapsed_time = time.time() - start_time
-    print(f"Elapsed Time: {elapsed_time:.4f} seconds")
-    while True:
-        print("Listening for user input...")
-        audio_data = record_audio(audio_file_name)
-        print("Voice detected. Transcribing...")
-        start_time = time.time()
-        text = transcribe(audio_file_name)
-        elapsed_time = time.time() - start_time
-        print(f"Elapsed Time: {elapsed_time:.4f} seconds")
-        print(f"Transcribed text: {text}")
-        response_text = generate_response(text)
-        print(f"Generated response: {response_text}")
-        response_audio = synthesize(response_text)
+    try:
+        print("Initializing...")
+        audio_file_name = "myrecording.wav"
+        while True:
+            print("Listening for user input...")
+            audio_data = record_audio(audio_file_name)
+            print("Voice detected. Transcribing...")
+            start_time = time.time()
+            text = transcribe(audio_file_name)
+            elapsed_time = time.time() - start_time
+            print(f"Elapsed Time: {elapsed_time:.4f} seconds")
+            print(f"Transcribed text: {text}")
+            response_text = generate_response(text)
+            print(f"Generated response: {response_text}")
+            response_audio = synthesize(response_text)
+
+    except KeyboardInterrupt:
+        print("\nExiting program.")
 
 
 if __name__ == "__main__":
