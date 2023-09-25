dd081a5a606a164c383490843c5ce33c210f9afb
Joey Wang <joey840404@gmail.com>
2023-09-24 19:42:38 -0700

    llm system prompt and memory

--- src/chat_utils.py
+++ src/chat_utils.py
@@ -1,7 +1,39 @@
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import SystemMessage
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
 
-def generate_response(text):
+system_prompt = """You are a personal assistan that's having a verbal conversation with a human.
+Add a little bit of filling words so it sounds like a natural converrsation.
+Your reply should be less then three sentences."""
 
-    llm = OpenAI()
-    response = llm.predict(text)
+
+def get_llm_chain():
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(content=system_prompt),  # The persistent system prompt
+            MessagesPlaceholder(
+                variable_name="chat_history"
+            ),  # Where the memory will be stored.
+            HumanMessagePromptTemplate.from_template(
+                "{human_input}"
+            ),  # Where the human input will injected
+        ]
+    )
+
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+    conversation_with_memory = LLMChain(
+        llm=ChatOpenAI(temperature=0), prompt=prompt, memory=memory
+    )
+    return conversation_with_memory
+
+
+def generate_response(llm, text):
+    response = llm.predict(human_input=text)
     return response
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sounddevice as sd
 from stt_tts import get_tts_engine, transcribe, synthesize
-from chat_utils import generate_response
+from chat_utils import get_llm_chain, generate_response
 from audio_utils import record_audio, play_audio
 import time
 
@@ -9,6 +9,7 @@ def main():
     try:
         print("Initializing...")
         audio_file_name = "myrecording.wav"
+        llm_chain = get_llm_chain()
         while True:
             print("Listening for user input...")
             audio_data = record_audio(audio_file_name)
@@ -18,7 +19,7 @@ def main():
             elapsed_time = time.time() - start_time
             print(f"Elapsed Time: {elapsed_time:.4f} seconds")
             print(f"Transcribed text: {text}")
-            response_text = generate_response(text)
+            response_text = generate_response(llm_chain, text)
             print(f"Generated response: {response_text}")
             response_audio = synthesize(response_text)
 
